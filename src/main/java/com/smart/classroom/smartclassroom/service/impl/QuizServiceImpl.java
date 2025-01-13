669b2307e971ae18ab13ef07e365ad40afc5d787
yasirurathnayaka <yasirurathnayaka97@gmail.com>
2025-01-14 00:55:34 +0530

    Add missing id in payloads

--- src/main/java/com/smart/classroom/smartclassroom/service/impl/QuizServiceImpl.java
+++ src/main/java/com/smart/classroom/smartclassroom/service/impl/QuizServiceImpl.java
@@ -132,6 +132,7 @@ public class QuizServiceImpl implements QuizService {
 
                     Set<QuizDTO> quizDTOs = quizzes.stream().map(
                                     q -> QuizDTO.builder()
+                                            .id(q.getId())
                                             .name(q.getName())
                                             .description(q.getDescription())
                                             .build()
